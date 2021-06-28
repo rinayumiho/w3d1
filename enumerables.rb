082d1e0b4575332b36266c279ff8f44e4080ad6c
gamestar <rinayumiho@gmail.com>
2021-06-28 16:21:07 -0400

    Long_round_1

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,28 @@
 ####### first line
+class Array
+    def my_each(&prc)
+        # (0...self.length).to_a
+        i = 0
+        while i < self.length
+            self[i] = prc.call(self[i])
+        end
+        self
+    end
+    
+    def my_select(&prc)
+        arr =[]
+        self.each do |ele|
+            arr << ele if prc.call(ele)
+        end
+        arr
+    end
+
+    def my_reject(&prc)
+        arr =[]
+        self.each do |ele|
+            arr << ele if !prc.call(ele)
+        end
+        arr
+    end
+end
+
