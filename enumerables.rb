4fe3216d6aacc1c0dcf80b85e81878a60fac7219
Quinn Mullaney <skiingqm@gmail.com>
2021-06-28 17:12:16 -0400

    Quinn 2nd Commit

--- enumerables.rb
+++ enumerables.rb
@@ -61,16 +61,21 @@ class Array
         arr
     end
 
+    def my_rotate(n = 1)
+        arr = []
+        n %= self.length
+        n += self.length if n < 0
+        (0...self.length).each {|i| arr << self[(i+n)%self.length]}
+        arr
+    end
     
 end
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-puts "*" * 20
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
+
+
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
